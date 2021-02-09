04c1ec0cfd72fab618d2dcba6d186151aca2d67d
Philipp Rappold <philipp.rappold@mollie.com>
2021-02-09 10:42:21 +0100

    Update logging fallback return value

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,5 +1,5 @@
-// eslint-disable-next-line no-console, @typescript-eslint/no-empty-function
-const log = process.env.NODE_ENV !== "test" ? console.log : () => {};
+// eslint-disable-next-line no-console
+const log = process.env.NODE_ENV !== "test" ? console.log : () => null;
 const error = (message: string) => log(`⛔️  ${message}`);
 const success = (message: string) => log(`✅  ${message}`);
 const info = (message: string) => log(`ℹ️  ${message}`);

90723458945fe47e22387a61a9da5bf41cb86b3c
Marino Linderhof <marino@mollie.com>
2021-06-15 17:48:12 +0200

    made the collection work agaiun

--- src/utils/convert-to-key-value.ts
+++ src/utils/convert-to-key-value.ts
@@ -3,7 +3,11 @@ import { ExportFileResponse } from "../types";
 export default (data: ExportFileResponse) => {
   const collection: { [key: string]: any } = {};
 
-  Object.keys(data).sort();
+  Object.keys(data)
+    .sort()
+    .forEach(key => {
+      collection[key] = data[key].message;
+    });
 
   return collection;
 };

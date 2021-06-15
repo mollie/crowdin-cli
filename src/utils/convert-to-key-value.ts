ae9205481f00999ed5373e474f0e11c80e680eb8
Marino Linderhof <marino@mollie.com>
2021-06-15 17:28:46 +0200

    remove-the-sanitizer-for-intl-support

--- src/utils/convert-to-key-value.ts
+++ src/utils/convert-to-key-value.ts
@@ -1,19 +1,9 @@
 import { ExportFileResponse } from "../types";
-import sanitize from "sanitize-html";
 
 export default (data: ExportFileResponse) => {
   const collection: { [key: string]: any } = {};
 
-  Object.keys(data)
-    .sort()
-    .forEach(key => {
-      collection[key] = sanitize(data[key].message, {
-        allowedAttributes: {
-          a: ["href", "name", "rel", "target"],
-        },
-        textFilter: text => text.replace("&amp;", "&"),
-      });
-    });
+  Object.keys(data).sort();
 
   return collection;
 };

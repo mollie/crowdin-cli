3f81a7b7bffa848e3091bda6b384ca424c856662
Philipp Rappold <philipp.rappold@mollie.com>
2024-01-15 16:10:50 +0100

    delete tasks when deleting branch

--- src/delete-branch.ts
+++ src/delete-branch.ts
@@ -1,4 +1,4 @@
-import { deleteBranch } from "./lib/crowdin";
+import { deleteBranch, deleteTask, listTasks } from "./lib/crowdin";
 import log from "./utils/logging";
 
 interface DeleteBranchOptions {
@@ -9,6 +9,16 @@ export default async (options: DeleteBranchOptions) => {
   log.info("Deleting branch from Crowdin");
 
   try {
+    // Delete any open tasks for this branch
+    const tasks = await listTasks();
+
+    // Iterate over tasks and delete any where the task name includes the branch name
+    for await (const task of tasks.data) {
+      if (task?.data?.description?.includes(options.branchName)) {
+        await deleteTask(task.data.id);
+      }
+    }
+
     await deleteBranch(options.branchName);
     log.success("Branch deleted");
   } catch (error) {
